ac52993168824d4243a1fd4054195346f7e14f0d
Vardhan Patil (DitherWither) <hi@vardhanpatil.com>
2024-08-09 01:19:17 +0530

    Add an example file

---
+++ examples/static_file.c
@@ -0,0 +1,22 @@
+#include <etch.h>
+
+// TODO automate build of examples
+
+ETCH_SERVER
+
+ETCH_INIT
+{
+        etch_set_port(8081);
+        printf("Hello, World\n");
+}
+
+ETCH_HANDLER
+{
+        EtchResponse res = etch_response_default();
+
+        if (etch_response_serve_file(&res, req.path) < 0) {
+                casprintf(&res.body, "<h1>404: Not Found</h1>");
+        }
+
+        return res;
+}
